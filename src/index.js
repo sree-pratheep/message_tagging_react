b028686d0a1b40e7431d9ff9f16e341a0d7f5148
Sree Pratheep <sreepratheep@gmail.com>
2021-07-29 17:45:02 +0530

    Render table with textbos

--- src/index.js
+++ src/index.js
@@ -2,133 +2,134 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-function Square(props) {
 
-    return (
-      <button 
-	    className="square" 
-	    onClick={props.onClick}
-      >
-	    {props.value}
-      </button>
-    );
+class TableCell extends React.Component {
+
+	constructor(props){
+		super(props)
+		this.handleClick = this.handleClick.bind(this)
+	}
+
+	handleClick = (event) =>{
+		console.log(event);
+		this.props.handleCellClick(this.props.cell.row, this.props.cell.column, event.target.value);
+	}
+
+	render(){
+		let proc_img_src = "data: image/png; base64, " + this.props.cell.processed_image
+		let img_src = "data: image/png; base64, " + this.props.cell.image
+		let text = this.props.cell.text
+		return(
+			<td>
+				<div><input type="text" name="test" defaultValue={text} onChange={this.handleClick}/></div>
+				<div><img src={img_src} alt="Couldn't load"/></div>
+				<div><img src={proc_img_src} alt="Couldn't load"/></div>
+			</td>
+		)
+	}
 }
 
-class Board extends React.Component {
 
-  handleClick(i) {    
-	  if ( this.state.squares[i] )
-		  return;
-	  const squares = this.state.squares.slice();    
-	  squares[i] = this.state.xIsNext? 'X': 'O';    
-	  this.setState({
-		  squares: squares,
-	  	  xIsNext: !this.state.xIsNext,
-	  });  
-  }
+function TableRow(props){
+	const row = props.row;
+	return(
+		<tr key={row.row}>
+			{row.map( (cell,index) => <TableCell key={index} cell={cell} handleCellClick={(row,column,value) => props.handleCellClick(row, column, value)}/>)}
+		</tr>
+	)
+}
 
 
-  renderSquare(i) {
-    return <Square 
-	  value={this.props.squares[i]} 
-	  onClick={() => this.props.onClick(i)}	  	
-		  />
-  };
+class Table extends React.Component {
+	constructor(props){
+		super(props);
+		this.state = {
+			isLoaded:false,
+			table_data:[]
+		}
+		this.updateCell = this.updateCell.bind(this)
+		this.confirmTableData = this.confirmTableData.bind(this)
+	}
+
+	updateCell(row, column, value){
+		const new_table = this.state.table_data.slice();
+		new_table[row][column].text = value;
+		this.setState({...this.state, table_data:new_table})
+	}
+
+	confirmTableData() {
+    const recipeUrl = 'http://localhost:8000/api/data/verified/table';
+    const postBody = {
+    	id : this.state.table_id,
+    	verified_cells : this.state.table_data.reduce((a,c) => a.concat(c), [])
+    };
+    const requestMetadata = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(postBody)
+    };
+    console.log(requestMetadata.body);
+    fetch(recipeUrl, requestMetadata)
+        .then(res => res.json())
+        .then(recipes => {
+            this.setState({ recipes });
+        });
+	}
+
+	componentDidMount() {
+		fetch("http://127.0.0.1:8000/api/parser/image/table/")
+		.then(res => res.json())
+		.then(
+			(result) => {
+				const table_id = result.telegram_media.id
+				const media_path = result.telegram_media.media_path
+				const table_data_id = result.id
+				const table_data = result.table_rows.map( row => {
+					return row.table_cells.map( cell => {
+						return {row:row.row, column:cell.column, text:cell.text, image:cell.image, processed_image:cell.processed_image}
+					})
+				})
+
+				this.setState({
+					isLoaded:true,
+					table_data:table_data,
+					table_id:result.id
+				});
+			},
+			(error) => {
+				this.setState({
+					isLoaded:true,
+					error:error
+				});
+			})
+	}
 
   render() {
+  	if (!this.state.isLoaded){
+  		return <div>Getting data from API</div>
+  	}
     return (
       <div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
+	    <table>
+      	<tbody>
+      		{
+      			this.state.table_data.map( (row, index) => <TableRow key={index} row={row} handleCellClick={(row,column,value) => this.updateCell(row, column, value)}/>)
+      		}
+      	</tbody>
+      </table>
+      <button onClick={this.confirmTableData}>Confirm Data</button>
       </div>
     );
   }
 }
 
-class Game extends React.Component {
-  constructor(props){
-	  super(props);
-	  this.state = {
-		history:[ {squares: Array(9).fill(null),
-		}],
-		xIsNext: false,
-		stepNumber: 0,
-	  };
-  }
-
-
-  jumpTo(step) {
-	  this.setState({
-		  stepNumber:step,
-		  xIsNext: (step % 2) === 0,
-	  });
-  }
-
-  handleClick(i) {
-	  const history = this.state.history.slice(0, this.state.stepNumber +1 );
-	  const current = history[history.length -1]
-          if ( current.squares[i] )
-                  return;
-          const squares = current.squares.slice();
-          squares[i] = this.state.xIsNext? 'X': 'O';
-          this.setState({
-		  history: history.concat([{squares:squares,}]),
-                  xIsNext: !this.state.xIsNext,
-		  stepNumber: history.length,
-          });
-  }
-
-
-
-  render() {
-    const history = this.state.history;
-    const current = history[this.state.stepNumber]
-    const moves = history.map((step,move) => {
-	    const desc = move? 'Go to move #' + move: 'Go to game start';
-	    const currentSquares = history[move].squares.map((x,index) => ( x!= null?index:null))
-	    const prevSquares = (move == 0)?currentSquares:history[move-1].squares.map((x,index) => ( x != null?index:null))
-	    const loc = (move !== 0)? ' (' + currentSquares.filter((x,index) => (x != null && x !== prevSquares[index])) + ', ' + (move%2==0?'X':'O') + ')':''
-	    const style = (move == this.state.stepNumber)? {fontWeight:'bold'}:{}
-	    return (
-		    <li key={move}>
-		    	<button onClick={() => this.jumpTo(move)} style={style}>{desc + loc }</button>
-		    </li>
-	    );});
-
-    return (
-      <div className="game">
-        <div className="game-board">
-          <Board 
-	    squares={current.squares} 
-	    onClick={(i) => this.handleClick(i)}
-	    />
-        </div>
-        <div className="game-info">
-          <div>{ 'Next player: ' + (this.state.xIsNext ? 'X':'O') }</div>
-          <ol>{ moves }</ol>
-        </div>
-      </div>
-    );
-  }
-}
 
 // ========================================
 
 ReactDOM.render(
-  <Game />,
+  <Table />,
   document.getElementById('root')
 );
 
